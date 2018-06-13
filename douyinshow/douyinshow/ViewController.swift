a81a4051637595800ea42d97629e984b09c6e5d7
fuyuan <fuy@alltosun.com>
2018-06-13 10:43:40 +0800

    new project

---
+++ douyinshow/douyinshow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  douyinshow
+//
+//  Created by fuyuan on 2018/6/13.
+//  Copyright © 2018年 fuyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
